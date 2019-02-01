1abd84e81fbaa559fc03ca3389ad5a1007a98d71
Kevin Woodland <kiwoodland@gmail.com>
2019-02-01 21:34:42 +0200

    Include build

---
+++ build/precache-manifest.8668f46e2f3497218daafe13ac4fe09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0184c5d8de49d2945431",
+    "url": "/static/js/main.0184c5d8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0184c5d8de49d2945431",
+    "url": "/static/css/main.72e3c777.chunk.css"
+  },
+  {
+    "revision": "430b14c4641d61ff95e01197dcd30b72",
+    "url": "/index.html"
+  }
+];
